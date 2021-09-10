b9f8388a81a635eea47e1e222ddd0e95a94f482e
slavo3dev <slavo@slavo3.com>
2021-09-10 18:00:06 -0400

    add some styles

--- content-script.js
+++ content-script.js
@@ -1,13 +1,41 @@
 window.onload = () => {
+    const label = document.createElement("label")
+    const textarea = document.createElement("TEXTAREA")
     const button = document.createElement('button');
-    button.id = "darkMode";
-    button.textContent = "Dark Mode"
+    const submitBtn = document.createElement('button')
+
+    button.id = "notes";
+    button.textContent = "Add Notes"
+
+    submitBtn.id = "saveNote";
+    submitBtn.textContent = "Save Note"
+
+    label.style.cssText = "color: white; font-size: 24px";
+    label.textContent = "Add Notes"
+
+    textarea.id = "textArea"
+    textarea.style.cssText = "font-size:12px; width: 99%; border: 1px solid #888; height: 10%"
+
+
     document.getElementById("end").prepend(button)
 
-    button.addEventListener("click", () => enableDarkMode())
+    button.addEventListener("click",() => {
+        
+        enableNotes(label,textarea,submitBtn);
+    })
+    
+    submitBtn.addEventListener("click", () => saveNote(textarea))
 }
 
 
-function enableDarkMode() {
-    document.getElementsByTagName("ytd-app")[0].style.background = "#000"
+function enableNotes(labal,textarea,submitBtn) {
+    
+    document.getElementsByTagName("ytd-app")[0].style.background = "rgba(255, 0, 0, 0.2)"
+    document.getElementById("secondary").prepend(submitBtn)
+    document.getElementById("secondary").prepend(textarea);
+    document.getElementById("secondary").prepend(labal);   
+}
+
+function saveNote(textarea) {
+    alert(textarea.value)
 }
